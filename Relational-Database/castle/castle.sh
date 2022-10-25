2890ff729155509800c4e2adcc4e776b70252cb3
LenSys <[omitted]>
2022-10-25 15:14:26 +0200

    added castle.sh for nano editor training

---
+++ Relational-Database/castle/castle.sh
@@ -0,0 +1,15 @@
+# created castle using nano editor
+# (training file for nano editor) 
+echo "
+Welcome to my castle
+"
+
+echo "
+ /\                /\ 
+/  \              /  \ 
+|  |______________|  |
+|                    |
+|   []    []    []   |
+|                    |
+|         __         |
+|________|  |________|
