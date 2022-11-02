07778ff4064887d7c50e74f7ba70ed57bcc678e1
LenSys <[omitted]>
2022-11-02 10:42:43 +0100

    added files for Request Header Parser Microservice

---
+++ Back-End-Development-and-APIs/Request-Header-Parser-Microservice/index.js
@@ -0,0 +1,36 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// API endpoint for IP address and other user info
+app.get('/api/whoami', function (req, res) {
+  let whoami = {
+    ipaddress: req.socket.remoteAddress,
+    language: req.get('Accept-Language'),
+    software: req.get('User-Agent')
+  };
+  
+  res.json(whoami);
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
